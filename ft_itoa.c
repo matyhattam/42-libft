2d735c3dfa80e4c743f036ebf88e3d99833ce299
maty <maty.hattam@gmail.com>
2026-03-06 15:24:27 +0100

    ft_itoa

---
+++ ft_itoa.c
@@ -0,0 +1,59 @@
+#include "libft.h"
+
+int num_size(int n) {
+  int i = 0;
+
+  if (n == 0)
+    return (1);
+
+  while (n != 0) {
+    n /= 10;
+    i++;
+  }
+  return (i);
+}
+
+void ft_rev_str(char *s) {
+  size_t s_len = ft_strlen(s);
+  size_t i = 0;
+  size_t j = s_len - 1;
+  char tmp;
+  while (i < s_len / 2) {
+    tmp = s[i];
+    s[i] = s[j];
+    s[j] = tmp;
+    i++;
+    j--;
+  }
+}
+
+char *ft_itoa(int n) {
+  size_t len = num_size(n);
+  char *nstr = malloc(len + 1);
+  if (!nstr)
+    return (NULL);
+
+  if (n == 0) {
+    nstr[0] = '0';
+    nstr[1] = '\0';
+    return (nstr);
+  }
+
+  long ui = (long)n;
+  if (n < 0)
+    ui *= -1;
+
+  size_t i = 0;
+  while (ui != 0) {
+    nstr[i] = (ui % 10) + '0';
+    ui /= 10;
+    i++;
+  }
+
+  if (n < 0)
+    nstr[i] = '-';
+  ft_rev_str(nstr);
+
+  nstr[i + 1] = '\0';
+  return (nstr);
+}
